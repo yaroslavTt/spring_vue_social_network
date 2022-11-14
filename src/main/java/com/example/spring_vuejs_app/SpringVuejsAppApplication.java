5494747574d4eeaf5dd7feb7204a5e281862761d
yaroslavTt <yaroslav.tykhovetskyi@gmail.com>
2022-11-14 15:02:26 +0200

    Initial commit

---
+++ src/main/java/com/example/spring_vuejs_app/SpringVuejsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_vuejs_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVuejsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVuejsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_vuejs_app/SpringVuejsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_vuejs_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVuejsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
